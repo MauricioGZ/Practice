b5baf743803364197ecf31c01a982b715c4f199a
Mauricio <mauricio.gomezzu@outlook.es>
2026-04-03 16:46:31 +0200

    some comments

--- include/List.hpp
+++ include/List.hpp
@@ -10,12 +10,18 @@ struct Node {
 
 struct List {
   Node* head;
+  //Constructor
   List ();
   List (Node* n);
+  //1. Destructor
   ~List ();
+  //2. Copy constructor
   List (const List& other);
+  //3. Move constructor
   List (List&& other) noexcept;
+  //4. Copy assignment
   List& operator= (const List& other);
+  //5. Move assignment
   List& operator= (List&& other) noexcept;
   void add (int value);
   void print (void) const;
